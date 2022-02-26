07c7869aeeeb472ba8abf9e93c8541f5e9acd2b4
wildwilliam2 <willwooten325@gmail.com>
2022-02-26 15:39:19 -0600

    Added comments to all functions in database.py; renamed the second test file for databases; changed bug in validate user that checked encrypted password before retreived; removed line encoding valid user pasword input; for get methods, if no queuery found return None; in crreate project, removed bug that searched hardware collection instead of project collection

---
+++ Database_Tester_2.py
@@ -0,0 +1,43 @@
+import Encryptor as e
+import Database as DB
+
+# Input: None
+# Output: None
+# Purpose: Test the functionality of Encryptor.py
+def test_encryptor():
+	test_num = 3
+	try:
+		test_num = int(input("Enter the number of tests to run: "))
+	except ValueError:
+		print("Invalid Input Will use 3 test")
+	for i in range(test_num):
+		try:
+			enc = e.Encryption(N = int(input("Enter a shift value: ")), D = int(input("Enter a direction (1 or -1): ")))	
+			test = input("Enter a text to encrypt: ")
+			enc_text = enc.encrypt(test)
+			print("Your text encrypted: ", enc_text)
+			dec_text  = enc.decrypt(enc_text)
+			print("Your text decrypted: ", dec_text)
+		except ValueError:
+			print("Invalid Input Try again")
+			continue
+	print("Done testing the encryption class")
+	return
+	
+	
+# Input: None
+# Output: None
+# Purpose: Test the functionality of Database.py
+def test_database():
+	return
+	
+
+def main():
+	print("Testing the Encryptor.py functions")
+	test_encryptor()
+	print("Testing the Database.py functions")
+	test_database()
+	
+	
+if __name__ == "__main__":
+	main()
