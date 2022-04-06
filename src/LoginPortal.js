ce7ce46c40f7d5bbb7aea6710e365a0df9a53065
Stephen Petrick <stephenp1912@gmail.com>
2022-04-06 01:42:55 -0500

    Added flask interaction from Will's tests
    Need to try testing with flask. Not sure if I implemented properly.
    Think I need to have the task app running alongisde the react app.

--- src/LoginPortal.js
+++ src/LoginPortal.js
@@ -10,20 +10,41 @@ const adminUser = {
 
 const [user, setUser] = useState({name: "", email: ""});
 const [error, setError] = useState("");
+const [serverResponse, setServerResponse] = useState("No Response yet");
 
+  async function sendCredentials(details){
+      let username = details.username;
+      let password = details.password;
+      let email = details.email;
+
+      // Fetch protocol
+       let dict = {
+         method : "POST",
+         body : JSON.stringify({
+           userid: username,
+           password: password
+         })
+       };
+       let res = await fetch("/verifyuser" , dict);
+       let responseJson = await res.json();
+     console.log(responseJson);
+
+     if(responseJson['valid']){
+       console.log("Logged in")
+       setUser({
+         name: details.name,
+         email: details.email
+       });
+       setServerResponse("User was verified");
+     }else{
+       console.log("Details do not match")
+       setError("Details do not match")
+       setServerResponse("User was not verified");
+     }
+  }  
 const Login = details => {
   console.log(details);
-
-  if(details.email == adminUser.email && details.password ==adminUser.password){
-    console.log("Logged in")
-    setUser({
-      name: details.name,
-      email: details.email
-    });
-  }else{
-    console.log("Details do not match")
-    setError("Details do not match")
-  }
+   sendCredentials(details);
 }
 
 const Logout = () => {
@@ -33,7 +54,7 @@ const Logout = () => {
 
 
   return (
-    <div className="App">
+    <div className="LoginPortal">
      {(user.email != "") ? (
        <div className="welcome">
          <h2>Welcome, <span>{user.name}</span></h2>
@@ -45,5 +66,5 @@ const Logout = () => {
     </div>
   );
 }
-
 export default LoginPortal;
+
